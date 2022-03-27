c16ba0524d439e15ff1f9632036a045ee1bfb841
Linh Vu <prodige112002@gmail.com>
2022-03-27 10:13:42 +0700

    simple responsive

--- app.js
+++ app.js
@@ -7,4 +7,18 @@ $(document).ready(function() {
             $(".header").removeClass("sticky-scroll");
         }
     })
+
+    
+
+    // scroll-bar
+    $(".progress .progress-value p").text("0%");
+    $(document).scroll(function() {
+        var scrollProgress = $(".progress");
+        var pos = $(document).scrollTop();
+        var calcHeight = $(document).height() - $(window).height();
+        var scrollValue = Math.round(pos * 100 / calcHeight);
+        
+        $(".progress .progress-value p").text(scrollValue + "%");
+        scrollProgress.css("background", "conic-gradient(var(--grey) " + scrollValue + "%, #2b2f38 " + scrollValue + "%)");
+    })
 })
